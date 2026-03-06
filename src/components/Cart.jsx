2438f4982d96b540859706803f8ae9f0ee2e9228
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-03-06 13:58:45 +0530

    Consuming the Context-2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,9 +3,9 @@ import { useContext } from "react";
 
 export default function Cart({ onUpdateItemQuantity }) {
 
-  const cartCtx = useContext(CartContext);
+  const {items} = useContext(CartContext);
 
-  const totalPrice = cartCtx.items.reduce(
+  const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0,
   );
@@ -13,8 +13,8 @@ export default function Cart({ onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
-      {cartCtx.items.length > 0 && (
+      {items.length === 0 && <p>No items in cart!</p>}
+      {items.length > 0 && (
         <ul id="cart-items">
           {items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
